4f53d651e9c7e095fdb5fa9db98dabd1b2ce19a4
tomas <tomas153lm@gmail.com>
2023-06-26 15:26:31 -0300

    feat: link js

---
+++ src/script.js
@@ -0,0 +1,12 @@
+var vidas = 5;
+var limite = 10;
+var numerosecreto = (Math.random() * (limite - 0) + 0);
+
+const adivinhar = (numero) => {
+    numero = parseInt(numero);
+    if(numero >= 0 && numero <= limite) {
+
+    } else {
+        
+    }
+}
